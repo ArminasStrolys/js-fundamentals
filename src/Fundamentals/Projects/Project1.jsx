e0978dfa5d8ee7aeb821f6e4522ca90b25bbc660
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-03 21:17:51 +0200

    Progress on 1st project

--- src/Fundamentals/Projects/Project1.jsx
+++ src/Fundamentals/Projects/Project1.jsx
@@ -1,28 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './project1.css';
 
 export const Project1 = () => {
+  const [score, setScore] = useState(20);
+  const [highscore, setHighscore] = useState(0);
+  const [userValue, setUserValue] = useState(null);
+  const [victory, setVictory] = useState(false);
+  const [message, setMessage] = useState('Start guessing...');
+
+  const [randomNum, setRandomNum] = useState(
+    Math.floor(Math.random() * 10 + 1)
+  );
+
+  console.log(randomNum);
+
+  const handleValueCheck = () => {
+    console.log(userValue);
+    handleGuessCheck();
+  };
+
+  const handleReset = () => {
+    setScore(20);
+    setRandomNum(Math.floor(Math.random() * 10 + 1));
+    setVictory(false);
+  };
+
+  const handleGuessCheck = () => {
+    if (userValue === randomNum) {
+      setMessage('Congrats!');
+      setVictory(true);
+      highscore < score && setHighscore(score);
+    } else if (userValue < randomNum) {
+      setMessage('Higher!');
+      setScore(score - 1);
+    } else {
+      setMessage('Lower!');
+      setScore(score - 1);
+    }
+  };
+
   return (
     <>
       <header>
         <h1>Guess My Number!</h1>
-        <p className="between">(Between 1 and 20)</p>
-        <button className="btn again">Again!</button>
-        <div className="number">?</div>
+        <p className="between">(Between 1 and {score})</p>
+        <button onClick={handleReset} className="btn again">
+          Again!
+        </button>
+        <div className="number">{victory ? randomNum : '?'}</div>
       </header>
       <main className="main-theme">
         <section className="left">
-          <input type="number" className="guess" />
-          <button className="btn check">Check!</button>
+          <input
+            type="number"
+            className="guess"
+            onChange={(e) => setUserValue(+e.target.value)}
+          />
+          <button className="btn check" onClick={handleValueCheck}>
+            Check!
+          </button>
         </section>
         <section className="right">
-          <p className="message">Start guessing...</p>
+          <p className="message">{message}</p>
           <p className="label-score">
-            💯 Score: <span className="score">20</span>
+            💯 Score: <span className="score">{score}</span>
           </p>
           <p className="label-highscore">
-            🥇 Highscore: <span className="highscore">0</span>
+            🥇 Highscore: <span className="highscore">{highscore}</span>
           </p>
         </section>
       </main>
