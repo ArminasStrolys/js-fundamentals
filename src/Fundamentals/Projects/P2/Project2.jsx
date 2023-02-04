1eed6c59d98bb7ec6ea4b431cfd16662958a228f
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-04 20:00:16 +0200

    Primitive modal open/close task/project done

--- src/Fundamentals/Projects/P2/Project2.jsx
+++ src/Fundamentals/Projects/P2/Project2.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './project2.css';
 
 export const Project2 = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const keyListener = ({ key }) => {
+      if (key === 'Escape') {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('keydown', keyListener, false);
+
+    return () => document.removeEventListener('keydown', keyListener);
+  }, []);
+
+  const handleOpen = () => {
+    setIsOpen(() => true);
+  };
+
   return (
     <div className="p2">
-      <button class="show-modal">Show modal 1</button>
-      <button class="show-modal">Show modal 2</button>
-      <button class="show-modal">Show modal 3</button>
+      <button onClick={handleOpen} class="show-modal">
+        Show modal 1
+      </button>
+      <button onClick={handleOpen} class="show-modal">
+        Show modal 2
+      </button>
+      <button onClick={handleOpen} class="show-modal">
+        Show modal 3
+      </button>
 
-      <div class="modal hidden">
-        <button class="close-modal">&times;</button>
-        <h1>I'm a modal window 😍</h1>
+      <div class={!isOpen ? 'modal hidden' : 'modal'}>
+        <button onClick={() => setIsOpen(false)} class="close-modal">
+          &times;
+        </button>
+        <h1>I'm a modal window</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -22,7 +47,10 @@ export const Project2 = () => {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
-      <div class="overlay hidden"></div>
+      <div
+        onClick={() => setIsOpen(false)}
+        class={isOpen ? 'overlay' : 'overlay hidden'}
+      ></div>
 
       <script src="script.js"></script>
     </div>
