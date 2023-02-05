2e167c1353f2a9d6818f9dd0dbd688e80a80a234
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-05 12:52:29 +0200

    Next project - roll the dice game starting files

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Assignments } from './Fundamentals/Assignments/Assignments';
 import { Basics } from './Fundamentals/Basics/Basics';
-import { Project1 } from './Fundamentals/Projects/Project1';
+import { Project1 } from './Fundamentals/Projects/P1/Project1';
 import { Project2 } from './Fundamentals/Projects/P2/Project2';
+import { Project3 } from './Fundamentals/Projects/P3/Project3';
 
 import './app.css';
 
@@ -23,12 +24,16 @@ function App() {
         <a className="nav-link" href="/p2">
           P2
         </a>
+        <a className="nav-link" href="/p3">
+          P3
+        </a>
       </div>
 
       <BrowserRouter>
         <Routes>
           <Route path={'/p1'} element={<Project1 />} />
           <Route path={'/p2'} element={<Project2 />} />
+          <Route path={'/p3'} element={<Project3 />} />
         </Routes>
       </BrowserRouter>
     </div>
