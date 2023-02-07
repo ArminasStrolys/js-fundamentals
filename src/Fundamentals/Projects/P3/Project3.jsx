a669594ecd3b42d2d4ecc2be5906a803fae2ffe3
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-07 16:42:49 +0200

    Minor fixes

--- src/Fundamentals/Projects/P3/Project3.jsx
+++ src/Fundamentals/Projects/P3/Project3.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './project3.css';
 import dice1 from './images/dice-1.png';
